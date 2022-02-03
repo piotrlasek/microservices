ff754b8463a0e6b32e0f84ab55e26d7e77810793
Piotr Lasek <piotr.lasek@dhc-vision.com>
2022-02-03 08:32:25 +0100

    initial commit of configuration-server and configuration folder

---
+++ config-server/src/test/java/com/piotrlasek/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piotrlasek.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
